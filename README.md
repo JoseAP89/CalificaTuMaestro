f88fcdfed7f2ae7ec4a7765a863207535de31937
Jose APumarino <josxe89@gmail.com>
2021-01-15 12:13:03 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Califica a Tu Maestro
---
+++ wwwroot/js/home.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.onscroll = function() {
+    setSticky()
+};
+
+let navbar = document.getElementById("searchTeacherBar");
+let sticky = navbar.offsetTop;
+
+function setSticky() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
